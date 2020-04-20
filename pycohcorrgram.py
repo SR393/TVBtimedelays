dfab88ddecde18c0e615e61678efd86b532f0ffd
SR393 <60163612+SR393@users.noreply.github.com>
2020-04-20 20:53:40 +1000

    Port of corrgram
    NOTE - currently calculates the CC "backwards" - results are -1* what they should be in general, unless signals are the same (i.e. autocorrelation).

---
+++ pycohcorrgram.py
@@ -0,0 +1,99 @@
+import numpy as np
+import pycorrgram as cg
+import scipy.signal as scp
+import matplotlib.pyplot as plt
+
+#I don't know who wrote this one
+def pyphase_nodes(y):
+
+    phases = np.zeros(y.shape)
+
+    for i in range(y.shape[0]):
+
+        cent = y[i] - np.mean(y[i])
+        ansig = scp.hilbert(cent)
+        phase1 = np.angle(ansig)
+        phase2 = np.unwrap(phase1)
+        phases[i] = phase2
+
+    return phases
+
+#correlation of coherence between sets of nodes n1 and n2 in y. y is a
+#2D array of time series, dimensions time series length X nodes (or
+#vice-versa). Optional arguments include opts dictionary containing
+#physical timestep, maximum lag, window length, overlap fraction (of window
+#length) and "inputphases", a Boolean object False by default, True if inputs
+#are phase signals. Also a Boolean object (True by default) indicating whether
+#outputs should be figure or dictionary of outputs.Originally written in MATLAB
+#by JA Roberts, QIMR Berghofer, 2018.
+def pycohcorrgram(*args):
+
+    y = args[0]
+    n1 = args[1]
+    n2 = args[2]
+    l = True
+
+    if y.shape[0] > y.shape[1]:
+
+        y = y.transpose() #Assumes more data points per time series than nodes
+    
+    if len(args) < 4:
+
+        opts = {"dt":1, "maxlag":30, "winlen":20, "overlapfrac":0.8,
+                "inputphases":False}
+    else:
+
+        opts = args[3]
+
+    if len(args) == 5:
+
+        l = args[4]
+
+    dt = opts["dt"]
+    maxlag = int(np.rint(opts["maxlag"]/dt))
+    winlen = int(np.rint(opts["winlen"]/dt))
+    overlapfrac = opts["overlapfrac"]
+    
+    #Check if signals are already phases
+    if opts["inputphases"]:
+        yphase = y
+    else:
+        yphase = pyphase_nodes(y)
+
+    #Coherences
+    ind1 = 1j*yphase[n1]
+    p1 = np.exp(ind1)
+    ind2 = 1j*yphase[n2]
+    p2 = np.exp(ind2)
+    coh1 = np.absolute(np.mean(p1, axis = 0))
+    coh2 = np.absolute(np.mean(p2, axis = 0))
+    
+    #Cross correlation
+    ovlp = np.floor(overlapfrac*winlen)
+    Coh = np.asarray(cg.pycorrgram(coh1, coh2, maxlag, winlen, ovlp))
+    
+    #Outputs
+    ct = Coh[2]*dt
+    cl = Coh[1]*dt
+    if l:
+
+        time = np.linspace(0, Coh[0].shape[1], 9)
+        lag = np.linspace(0, 2*maxlag + 1, 9)
+        
+        T = np.linspace(0, ct[-1], 9)
+        L = np.linspace(-maxlag, maxlag, 9)
+        
+        fig = plt.matshow(Coh[0], aspect="auto")
+        plt.xticks(time, T)
+        plt.yticks(lag, L)
+        plt.xlabel('Time')
+        plt.ylabel('Lag')
+        plt.colorbar()
+        plt.show()
+
+    else:
+
+        out = {"cc": Coh[0], "cl":cl, "ct":ct, "opts":opts, "n1":n1, "n2":n2}
+
+        return out
+        
---
+++ pycorrgram.py
@@ -0,0 +1,159 @@
+#Translation of corrgram function in MATLAB for windowed time lagged
+#cross-correlation of two time series, originally written by Norbert Marwan,
+#Potsdam University, Germany
+
+#Input time series y1 and y2 for which the cross-correlation is to be calculated
+#as first two args, assumes ndarray object.
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+#define function to standardise windows later on
+def normalise(A):
+
+    N = np.zeros(A.shape)
+
+    mu = np.mean(A, axis = 1)
+    std = np.std(A, axis = 1, dtype=np.float64, ddof = 1)
+
+    for i in range(A.shape[0]):
+        
+        cent = A[i] - mu[i]
+        norm = cent/std[i]
+        N[i] = norm
+
+    return N
+
+#define (simpler) function to stand in for MATLAB's 'buffer' with no delay
+def pybuffer(A, n, o):
+
+    nw = int(np.ceil((len(A)-o)/(n-o)))
+    B = np.zeros((nw, n))
+
+    i = 0
+
+    while i < nw - 1:
+        
+        j = i*(n - o)
+        B[i] = A[j:j+n]
+        i = i + 1
+        
+    j = (i)*(n-o)
+    r = len(A) - (i-1)*(n-o) - n
+    
+    B[nw-1] = np.append(A[j:j+o+r], [0]*(n-o-r))
+
+    return B
+
+def pycorrgram(*args):
+
+    x = args[0]
+    y = args[1]
+
+    nx = int(len(x))
+    ny = int(len(y))
+
+    if nx < ny:
+
+        x.append(x, np.zeros(ny - nx))
+
+    if ny < nx:
+
+        np.append(y, np.zeros(nx - ny))
+
+    maxlag = int(np.floor(nx/10))
+    window = int(np.floor(nx/10))
+    overlap = 0
+
+    if len(args) > 2:
+
+        maxlag = int(args[2])
+        
+        if maxlag < 0:
+            raise ValueError("Requires positive integer value for maximum lag")
+
+        if len(args) > 3:
+
+            window = int(args[3])
+
+            if window < 0:
+                raise ValueError("Requires positive integer value for window length")
+
+            if len(args) > 4:
+
+                overlap = int(args[4])
+
+                if overlap < 0:
+                    raise ValueError("Requires positive integer value for overlap")
+                if overlap >= window:
+                    raise ValueError("Requires overlap to be strictly less than window length")
+        
+    #Create time delayed signals
+    X = np.zeros((maxlag + 1, nx))
+    Y = np.zeros((maxlag + 1, ny))
+
+    i = 0
+
+    while i <= maxlag:
+        
+        LSX = np.append(np.zeros(maxlag - i), x[:nx-maxlag+i])
+        LSY = np.append(np.zeros(i), y[:ny-i])
+        
+        X[i] = LSX
+        Y[i] = LSY
+        i = i+1
+    
+
+    #Divide unshifted signals into windows and normalise
+    Xw = pybuffer(x, window, overlap)
+    Yw = pybuffer(y, window, overlap)
+
+    Xw = normalise(Xw)
+    Yw = normalise(Yw)
+    
+    #Perform cross-correlation
+    C = np.zeros((2*maxlag + 1, int(np.ceil((nx - overlap)/(window - overlap)))))
+    cnt = 0
+    
+    for i in range(X.shape[0]):
+        
+        XiS = X[i]
+        Xi = pybuffer(XiS, window, overlap)
+        Xi = normalise(Xi)
+        R = np.mean(Yw*Xi, axis = 1)
+        C[cnt] = R
+        cnt = cnt + 1
+
+
+    for i in range(1, Y.shape[0]):
+    
+        YiS = Y[i]
+        Yi = pybuffer(YiS, window, overlap)
+        Yi = normalise(Yi)
+        R = np.mean(Xw*Yi, axis = 1)
+        C[cnt] = R
+        cnt = cnt + 1
+        
+    #Outputs
+    if args == 2:
+        
+        t = np.arange(0, nx+(window-overlap), window - overlap)
+        lag = np.linspace(-maxlag, maxlag, 2*maxlag + 1)
+
+
+        time = np.arange(np.ceil((nx - overlap)/(window - overlap)))
+        T = np.linspace(0, nx - window, Xw.shape[0])
+        l = np.linspace(0, 2*maxlag+1, 9)
+        L = np.linspace(-maxlag, maxlag, 9)
+            
+        fig = plt.matshow(C, aspect="auto")
+        plt.xticks(time, T)
+        plt.yticks(l, L)
+        plt.xlabel('Time')
+        plt.ylabel('Lag')
+        plt.colorbar()
+        plt.show()
+
+    return [C, lag, t]
+    
+    
