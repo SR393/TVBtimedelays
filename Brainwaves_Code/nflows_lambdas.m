882df16d93533a4ee1b88ddf6d59bbc4da08077e
SR393 <60163612+SR393@users.noreply.github.com>
2020-10-21 22:58:34 +1000

    Update and rename nflows_lambdas.m to Brainwaves_Code/nflows_lambdas.m

--- Brainwaves_Code/nflows_lambdas.m
+++ Brainwaves_Code/nflows_lambdas.m
@@ -1,6 +1,9 @@
+%starts process of running simulation data through neural-flows
+%by choosing parameter set (K-s, lambda-k, or lambda-I) and running
+%nflows_multirun()
 function nflows_lambdas(Il, spc, kl)
 filestrings_lambdas = ["lambda50", "lambda100", "lambda150", "lambda200", "lambda250", "lambda300"];
-filestrings_couplings = ["coupling07", "coupling08"];
+filestrings_couplings = ["coupling05", "coupling06", "coupling07", "coupling08"];
 if Il
     for k = 1:length(filestrings_lambdas)
         nflows_multirun('k-lambda\\', filestrings_lambdas(k), Il, spc, kl)
